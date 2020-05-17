c5dc91e0b511b60681675f7ca2a056b4c0c4e160
Tenax30 <mrbord30@yandex.ru>
2020-05-17 12:30:23 +0300

    add NotFound page for for invalid id of current request

--- InnostageTest/Controllers/RequestsController.cs
+++ InnostageTest/Controllers/RequestsController.cs
@@ -72,6 +72,11 @@ namespace InnostageTest.Controllers
                 .Include(r => r.RequestStatus)
                 .FirstOrDefault(r => r.RequestId == id);
 
+            if(request == null)
+            {
+                return NotFound();
+            }
+
             return View(request);
         }
     }
